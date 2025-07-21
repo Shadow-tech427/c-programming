c7ffba71118603bf18f8f8867b0216e7ff37f2cb
Shadow-tech427 <boratenakul5@gmail.com>
2025-07-21 23:49:32 +0530

    Q9

---
+++ Assignment2.c/Q9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == year && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is a not leap year.\n", year);
+    }
+    return 0;
+}
