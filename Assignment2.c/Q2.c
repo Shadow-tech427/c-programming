e4943171576e72206effbcb22a39bb7ab9c34384
Shadow-tech427 <boratenakul5@gmail.com>
2025-07-21 18:49:21 +0530

    Q2

---
+++ Assignment2.c/Q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter a Number: ");
+    scanf("%d", &num);
+
+    if (num>0){
+        printf("The number %d is positive.\n", num);
+    } else if (num<0){
+        printf("The number %d is negative.\n", num);
+    } else {
+        printf("The number %d is zero.\n");
+    }
+
+    return 0;
+}
